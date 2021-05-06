e6a5d6f22580b6a0a3b23304358e4131bafeb23a
fadlisabda1 <fadlisabda1@yahoo.com>
2021-05-06 22:53:45 +0700

    menghapus mylocation pada mycontact

--- app/Controllers/Contact.php
+++ app/Controllers/Contact.php
@@ -11,8 +11,7 @@ class Contact extends BaseController
             'mycontact' => [
                 [
                     'github' => 'https://github.com/fadlisabda',
-                    'instagram' => 'https://www.instagram.com/fadlisabda01/',
-                    'kota' => 'https://www.google.com/maps/place/Pekanbaru,+Kota+Pekanbaru,+Riau/@0.5137908,101.3711354,12z/data=!3m1!4b1!4m5!3m4!1s0x31d5ab80690ee7b1:0x94dde92c3823dbe4!8m2!3d0.5070677!4d101.4477793'
+                    'instagram' => 'https://www.instagram.com/fadlisabda01/'
                 ]
             ]
         ];
--- app/Views/contact/index.php
+++ app/Views/contact/index.php
@@ -6,7 +6,6 @@
             <h2>My Contact</h2>
             <?php foreach ($mycontact as $m) : ?>
                 <div class="list-group">
-                    <a href="<?= $m['kota'] ?>" class="list-group-item list-group-item-action" target="_blank">My Location</a>
                     <a href="<?= $m['github'] ?>" class="list-group-item list-group-item-action" target="_blank">My Github</a>
                     <a href="<?= $m['instagram'] ?>" class="list-group-item list-group-item-action" target="_blank">My Instagram</a>
                 </div>
