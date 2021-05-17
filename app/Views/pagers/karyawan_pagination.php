24aebf445b31966c0ad38edd205ee10907c67c37
fadlisabda1 <fadlisabda1@yahoo.com>
2021-05-17 16:54:24 +0700

    merubah fungsi link pada pagination

--- app/Views/pagers/karyawan_pagination.php
+++ app/Views/pagers/karyawan_pagination.php
@@ -1,15 +1,15 @@
 <?php $pager->setSurroundCount(2) ?>
 
-<nav aria-label="Page navigation">
+<nav aria-label="<?= lang('Pager.pageNavigation') ?>">
     <ul class="pagination">
-        <?php if ($pager->hasPrevious()) : ?>
+        <?php if ($pager->hasPreviousPage()) : ?>
             <li class="page-item">
                 <a class="page-link" href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
                     <span aria-hidden="true"><?= lang('Pager.first') ?></span>
                 </a>
             </li>
             <li class="page-item">
-                <a class="page-link" href="<?= $pager->getPrevious() ?>" aria-label="<?= lang('Pager.previous') ?>">
+                <a class="page-link" href="<?= $pager->getPreviousPage() ?>" aria-label="<?= lang('Pager.previous') ?>">
                     <span aria-hidden="true"><?= lang('Pager.previous') ?></span>
                 </a>
             </li>
@@ -23,9 +23,9 @@
             </li>
         <?php endforeach ?>
 
-        <?php if ($pager->hasNext()) : ?>
+        <?php if ($pager->hasNextPage()) : ?>
             <li class="page-item">
-                <a class="page-link" href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
+                <a class="page-link" href="<?= $pager->getNextPage() ?>" aria-label="<?= lang('Pager.next') ?>">
                     <span aria-hidden="true"><?= lang('Pager.next') ?></span>
                 </a>
             </li>
