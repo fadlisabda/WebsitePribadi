f6af8394796e4a78a765d8e492961c6d9bfa3d06
fadlisabda1 <fadlisabda1@yahoo.com>
2021-06-09 10:20:28 +0700

    menghapus folder bootstrap dan menghapus external css

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -9,9 +9,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <!-- My Css -->
-    <!-- <link rel="stylesheet" href="/bootstrap/css/style.css"> -->
-
     <title><?= $title; ?></title>
 </head>
 
